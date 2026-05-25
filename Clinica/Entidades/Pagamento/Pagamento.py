a52f285c39ee1d8ddf6361d67c47744cea8e1ff7
Davi Koscianski Moreira <davikiko08@gmail.com>
2026-05-25 15:53:38 -0300

    Refactor Pagamento class to include atendimento and paciente

--- Clinica/Entidades/Pagamento/Pagamento.py
+++ Clinica/Entidades/Pagamento/Pagamento.py
@@ -1,18 +1,41 @@
 from abc import ABC
 from datetime import date
+
 class Pagamento(ABC):
-    def __init__(self, data: date, valor_pago: float):
+    def __init__(self, data: date, atendimneto: "Atendimento", paciente: "Paciente", valor_pago: float):
         self.__data = data
+        self.__atendimento = atendimento
+        self.__paciente = paciente
         self.__valor_pago = valor_pago
 
     @property
-    def data(self): return self.__data
+    def data(self): 
+        return self.__data
 
     @data.setter
-    def data(self, data: date): self.__data = data
+    def data(self, data: date): 
+        self.__data = data
 
     @property
-    def valor_pago(self): return self.__valor_pago
+    def atendimento(self): 
+        return self.__atendimento
+
+    @atendimento.setter
+    def atendimento(self, atendimento: "Atendimento"): 
+        self.__atendimento = atendimento
+
+    @property
+    def paciente(self): 
+        return self.__paciente
+
+    @paciente.setter
+    def paciente(self, paciente: "Paciente"): 
+        self.__paciente = paciente
+
+    @property
+    def valor_pago(self): 
+        self.__valor_pago
 
     @valor_pago.setter
-    def valor_pago(self, valor_pago: float): self.__valor_pago = valor_pago
+    def valor_pago(self, valor_pago: float): 
+        self.__valor_pago = valor_pago
