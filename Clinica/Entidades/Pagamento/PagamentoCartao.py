95565f582238839bd1199ae6ce62a60e769f02bb
Davi Koscianski Moreira <davikiko08@gmail.com>
2026-05-25 16:00:15 -0300

    Update PagamentoCartao constructor to include Atendimento and Paciente

--- Clinica/Entidades/Pagamento/PagamentoCartao.py
+++ Clinica/Entidades/Pagamento/PagamentoCartao.py
@@ -2,8 +2,8 @@ from datetime import date
 import Clinica.Entidades.Pagamento.Pagamento as Pagamento
 
 class PagamentoCartao(Pagamento):
-    def __init__(self, data: date, valor_pago: float, numero_cartao: str, bandeira: str):
-        super().__init__(data, valor_pago)
+    def __init__(self, data: date, atendimento: Atendimento, paciente: Paciente, valor_pago: float, numero_cartao: str, bandeira: str):
+        super().__init__(data, atendimento, paciente, valor_pago)
         self.__numero_cartao = numero_cartao
         self.__bandeira = bandeira
 
