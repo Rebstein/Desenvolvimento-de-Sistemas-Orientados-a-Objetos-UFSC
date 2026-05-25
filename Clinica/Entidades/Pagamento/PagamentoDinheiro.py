9f2f24205d40b1c482e19402e21c7a1e570816c7
Davi Koscianski Moreira <davikiko08@gmail.com>
2026-05-25 16:20:43 -0300

    Fix constructor parameters in PagamentoDinheiro

--- Clinica/Entidades/Pagamento/PagamentoDinheiro.py
+++ Clinica/Entidades/Pagamento/PagamentoDinheiro.py
@@ -2,5 +2,5 @@ from datetime import date
 import Clinica.Entidades.Pagamento.Pagamento as Pagamento
 
 class PagamentoDinheiro(Pagamento):
-    def __init__(self, data: date, atendimento: Atendimento, paciente: Paciente valor_pago: float):
-        super().__init__(data, valor_pago)
+    def __init__(self, data: date, atendimento: Atendimento, paciente: Paciente, valor_pago: float):
+        super().__init__(data, atendimento, paciente, valor_pago)
