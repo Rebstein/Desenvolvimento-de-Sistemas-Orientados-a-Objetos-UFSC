241f4530e27e9bd7a4804e50b9920a57661396f4
Davi Koscianski Moreira <davikiko08@gmail.com>
2026-05-25 16:01:37 -0300

    Modify constructor to include atendimento and paciente

--- Clinica/Entidades/Pagamento/PagamentoDinheiro.py
+++ Clinica/Entidades/Pagamento/PagamentoDinheiro.py
@@ -2,5 +2,5 @@ from datetime import date
 import Clinica.Entidades.Pagamento.Pagamento as Pagamento
 
 class PagamentoDinheiro(Pagamento):
-    def __init__(self, data: date, valor_pago: float):
+    def __init__(self, data: date, atendimento: Atendimento, paciente: Paciente valor_pago: float):
         super().__init__(data, valor_pago)
